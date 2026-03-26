e6c59f6d5e96f0ac90cc9bfd2b08d9effb7cb817
Rajarshi Tambuli <rajarshitambuli@gmail.com>
2026-03-26 15:38:55 +0530

    feat: add scrolltotop component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Global/Navbar';
 import Footer from './Components/Global/Footer';
+import ScrollToTop from './Components/Global/ScrollToTop';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Product from './Pages/Product/Product';
@@ -10,6 +11,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow pt-20">
